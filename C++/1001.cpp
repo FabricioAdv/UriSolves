062bf82dd971686f6481dea134f38a5b86b237dd
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-05 09:54:26 -0300

    Update 1001.cpp

--- C++/1001.cpp
+++ C++/1001.cpp
@@ -2,13 +2,13 @@
  
 using namespace std;
  
-int main() {
- 
-    int x[2]; 
+int main() 
+{
+    	int x[2]; 
 	
 	cin >> x[0] >> x[1];
 	
 	cout << "X = " << x[0]+x[1] << endl;
  
-    return 0;
+    	return 0;
 }
