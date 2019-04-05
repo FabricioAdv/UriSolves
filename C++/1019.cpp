695f86cc1e647d67718e2f52ece1fa9a2ea7adf7
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-05 15:05:15 -0300

    Create 1019.cpp

---
+++ C++/1019.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x, y[3];
+    double aux;
+
+    cin >> x;
+    
+    y[0] = x / 3600.0;
+    y[1] = ((x / 3600.0) - y[0]) * 60.0;
+    y[2] = ((((x / 3600.0) - y[0]) * 60) - y[1]) * 60.0 + 0.0001;
+    
+    cout << y[0] << ":" << y[1] << ":" << y[2] << endl;
+ 
+    return 0;
+}
