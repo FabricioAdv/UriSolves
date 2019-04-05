07b155334bbe919808ba9091689a4e6a7ed34b88
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-05 14:59:20 -0300

    Create 1004.cpp

---
+++ C++/1004.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x[2], y; 
+	
+	cin >> x[0] >> x[1];
+	
+	y = x[0] * x[1];
+	
+	cout << "PROD = " << y << endl;
+ 
+    return 0;
+}
