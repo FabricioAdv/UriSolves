b79c6abc6f8b63f34a14ebfce4c02c2f83f33603
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-05 15:02:46 -0300

    Create 1013.cpp

---
+++ C++/1013.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x[3], y;
+
+    y = 0;
+
+    for (int i = 0 ; i < 3 ; i++)
+    {
+        cin >> x[i];
+
+        if (x[i] > y)
+        {
+            y = x[i];
+        }
+    }
+
+    cout << y << " eh o maior" << endl;
+ 
+    return 0;
+}
