abeca326da7169e092d269865c0dcbdf29c2a256
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-05 15:00:20 -0300

    Create 1007.cpp

---
+++ C++/1007.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x[4], y; 
+	
+	cin >> x[0] >> x[1] >> x[2] >> x[3];
+	
+	y = (x[0] * x[1] - x[2] * x[3]);
+	
+	cout << "DIFERENCA = " << y << endl;
+ 
+    return 0;
+}
