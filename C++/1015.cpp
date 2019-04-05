c15ad890e604a48add4f318a0bb6a7f1c7cfbb4e
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-05 15:03:27 -0300

    Create 1015.cpp

---
+++ C++/1015.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x[4];
+    
+    cin >> x[0] >> x[1] >> x[2] >> x[3];
+    
+    cout << fixed;
+    cout.precision(4);
+    cout << sqrt(pow(x[2] - x[0], 2) + pow(x[3] - x[1], 2)) << endl;
+
+	return 0;
+}
