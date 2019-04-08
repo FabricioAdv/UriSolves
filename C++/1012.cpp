cd2afc534e0c1a26c9eb1a33bdde41601c9bcf38
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-08 15:57:15 -0300

    Update 1012.cpp

--- C++/1012.cpp
+++ C++/1012.cpp
@@ -2,21 +2,20 @@
  
 using namespace std;
  
-int main() {
- 
-    double x[3], pi;
+int main() 
+{ 
+    	double x[3], pi;
 	
 	pi = 3.14159;
 	
 	cin >> x[0] >> x[1] >> x[2];
 	
-	cout << fixed;
 	cout.precision(3);	
-	cout << "TRIANGULO: " << (x[0] * x[2]) / 2 << endl;
-	cout << "CIRCULO: " << pi * (x[2] * x[2]) << endl;
-	cout << "TRAPEZIO: " << ((x[0] + x[1]) * x[2]) / 2 << endl;
-	cout << "QUADRADO: " << x[1] * x[1] << endl;
-	cout << "RETANGULO: " << x[0] * x[1] << endl;
+	cout << fixed << "TRIANGULO: " << (x[0] * x[2]) / 2 << endl;
+	cout << fixed << "CIRCULO: " << pi * (x[2] * x[2]) << endl;
+	cout << fixed << "TRAPEZIO: " << ((x[0] + x[1]) * x[2]) / 2 << endl;
+	cout << fixed << "QUADRADO: " << x[1] * x[1] << endl;
+	cout << fixed << "RETANGULO: " << x[0] * x[1] << endl;
  
-    return 0;
+    	return 0;
 }
