923aa5c11119cf3491cd3d9a60c6b465487050a1
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-08 16:02:21 -0300

    Update 1021.cpp

--- C++/1021.cpp
+++ C++/1021.cpp
@@ -2,52 +2,52 @@
  
 using namespace std;
  
-int main() {
- 
-    double valor;
-    int x[12], y[12], notas, moedas;
+int main() 
+{ 
+    	double valor;
+    	int x[12], y[12], notas, moedas;
 
-    cin >> valor;
+    	cin >> valor;
     
-    notas = valor;
-    moedas = (valor - notas) * 100;
+    	notas = valor;
+    	moedas = (valor - notas) * 100;
     
-    x[0] = notas / 100;
-    y[0] = notas % 100;
+    	x[0] = notas / 100;
+    	y[0] = notas % 100;
     
-    x[1] = y[0] / 50;
-    y[1] = y[0] % 50;
+    	x[1] = y[0] / 50;
+    	y[1] = y[0] % 50;
     
-    x[2] = y[1] / 20;
-    y[2] = y[1] % 20;
+    	x[2] = y[1] / 20;
+    	y[2] = y[1] % 20;
     
-    x[3] = y[2] / 10;
-    y[3] = y[2] % 10;
+    	x[3] = y[2] / 10;
+    	y[3] = y[2] % 10;
     
-    x[4] = y[3] / 5;
-    y[4] = y[3] % 5;
+    	x[4] = y[3] / 5;
+    	y[4] = y[3] % 5;
     
-    x[5] = y[4] / 2;
-    y[5] = y[4] % 2;
+    	x[5] = y[4] / 2;
+    	y[5] = y[4] % 2;
     
-    x[6] = y[5];
-    y[6] = y[5];
+    	x[6] = y[5];
+    	y[6] = y[5];
     
-    x[7] = moedas / 50;
-    y[7] = moedas % 50;
+    	x[7] = moedas / 50;
+    	y[7] = moedas % 50;
     
-    x[8] = y[7] / 25;
-    y[8] = y[7] % 25;
+    	x[8] = y[7] / 25;
+    	y[8] = y[7] % 25;
     
-    x[9] = y[8] / 10;
-    y[9] = y[8] % 10;
+    	x[9] = y[8] / 10;
+    	y[9] = y[8] % 10;
     
-    x[10] = y[9] / 5;
-    y[10] = y[9] % 5;
+    	x[10] = y[9] / 5;
+    	y[10] = y[9] % 5;
     
-    x[11] = y[10];
+    	x[11] = y[10];
     
-    cout << "NOTAS:" << endl;
+    	cout << "NOTAS:" << endl;
 	cout << x[0] << " nota(s) de R$ 100.00" << endl;
 	cout << x[1] << " nota(s) de R$ 50.00" << endl;
 	cout << x[2] << " nota(s) de R$ 20.00" << endl;
@@ -62,5 +62,5 @@ int main() {
 	cout << x[10] << " moeda(s) de R$ 0.05" << endl;
 	cout << x[11] << " moeda(s) de R$ 0.01" << endl;
  
-    return 0;
+    	return 0;
 }
