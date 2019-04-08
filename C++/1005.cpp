51afb4e663940753f41d3ee793ede8d9b1a132ab
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-08 15:52:26 -0300

    Update 1005.cpp

--- C++/1005.cpp
+++ C++/1005.cpp
@@ -2,17 +2,16 @@
  
 using namespace std;
  
-int main() {
- 
-    double x[2], y; 
+int main() 
+{ 
+    	double x[2], y; 
 	
 	cin >> x[0] >> x[1];
 	
 	y = (x[0] * 0.35 + x[1] * 0.75) / 1.1;
 	
-	cout << fixed;
 	cout.precision(5);
-	cout << "MEDIA = " << y << endl;
+	cout << fixed << "MEDIA = " << y << endl;
  
-    return 0;
+    	return 0;
 }
