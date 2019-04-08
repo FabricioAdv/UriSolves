950dc28f52641acd407e3e20a017c9c48495c714
Fabricio Lima <fabricio_lima_carvalho@hotmail.com>
2019-04-08 15:53:07 -0300

    Update 1006.cpp

--- C++/1006.cpp
+++ C++/1006.cpp
@@ -2,17 +2,16 @@
  
 using namespace std;
  
-int main() {
- 
-    double x[3], y; 
+int main() 
+{ 
+    	double x[3], y; 
 	
 	cin >> x[0] >> x[1] >> x[2];
 	
 	y = (x[0] * 0.2 + x[1] * 0.3 + x[2] * 0.5);
 	
-	cout << fixed;
 	cout.precision(1);
-	cout << "MEDIA = " << y << endl;
+	cout << fixed << "MEDIA = " << y << endl;
  
-    return 0;
+    	return 0;
 }
